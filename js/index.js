fb45832ba3b03530ee1cfd38f97ab351f829fcdf
zyp0603 <752498583@qq.com>
2018-05-30 13:40:27 +0800

    移动营业厅2

---
+++ js/index.js
@@ -0,0 +1,32 @@
+var mySwiper = new Swiper ('.swiper-container', {
+
+    effect : 'coverflow',
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    initialSlide:4,
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
